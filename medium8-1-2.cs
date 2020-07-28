9bf2d28275f7de9682f915c7797bd91021c4e555
Sergey Starukhin <servalstar@gmail.com>
2020-07-28 18:25:47 +0400

    version 2

--- medium8-1-2.cs
+++ medium8-1-2.cs
@@ -7,10 +7,10 @@ namespace Task
     {
         public static void Main(string[] args)
         {
-            MovingObjectData[] movingObjects = {
-                new MovingObjectData(new Vector2(5, 5)),
-                new MovingObjectData(new Vector2(10, 10)),
-                new MovingObjectData(new Vector2(15, 15)) };
+            List<MovingObject> movingObjects = new List<MovingObject>() {
+                new MovingObject(new Vector2(5, 5)),
+                new MovingObject(new Vector2(10, 10)),
+                new MovingObject(new Vector2(15, 15)) };
 
             TrajectorySimulator trajectorySimulator = new TrajectorySimulator(movingObjects);
             trajectorySimulator.Simulate();
@@ -20,11 +20,11 @@ namespace Task
 
     public class TrajectorySimulator
     {
-        private MovingObjectData[] movingObjects;
+        private List<MovingObject> movingObjects;
         private ConsoleWriter consoleWriter;
         private Random random;
 
-        public TrajectorySimulator(MovingObjectData[] objects)
+        public TrajectorySimulator(List<MovingObject> objects)
         {
             movingObjects = objects;
             consoleWriter = new ConsoleWriter();
@@ -35,74 +35,60 @@ namespace Task
         {
             while (true)
             {
-                ValidateAndSetObjectsStates();
-                ShiftObjectsPosition();
+                RemoveCrossedObjects();
+                SimulateMovement();
                 SendPositionsToConsole();
             }
         }
 
-        private void ValidateAndSetObjectsStates()
+        private void RemoveCrossedObjects()
         {
-            for (int i = 0; i < movingObjects.Length; i++)
-            {
-                int j = i + 1;
+            List<MovingObject> nonCrossedObjects = new List<MovingObject>();
 
-                while (j < movingObjects.Length)
-                {
-                    Vector2 pos1 = movingObjects[i].Position;
-                    Vector2 pos2 = movingObjects[j].Position;
+            foreach (var movingObject in movingObjects)
+                if (CheckCrossingObjects(movingObject, movingObjects) == false)
+                    nonCrossedObjects.Add(movingObject);
 
-                    if (pos1.x == pos2.x && pos1.y == pos2.y)
-                    {
-                        movingObjects[i].KillObject();
-                        movingObjects[j].KillObject();
-                    }
-
-                    j++;
-                }
-            }
+            movingObjects = nonCrossedObjects;
         }
 
-        private void ShiftObjectsPosition()
+        private bool CheckCrossingObjects(MovingObject checkedObject, List<MovingObject> objects)
+        {
+            for (int i = objects.IndexOf(checkedObject) + 1; i < objects.Count; i++)
+                if (checkedObject.Position == objects[i].Position)
+                    return true;
+
+            return false;
+        }
+
+        private void SimulateMovement()
         {
             foreach (var obj in movingObjects)
-            {
-                if (obj.IsAlive)
-                    obj.ShiftPosition(new Vector2(random.Next(-1, 1), random.Next(-1, 1)));
-            }
+                obj.ShiftPosition(new Vector2(random.Next(-1, 1), random.Next(-1, 1)));
         }
 
         private void SendPositionsToConsole()
         {
-            for (int i = 0; i < movingObjects.Length; i++)
-            {
-                if (movingObjects[i].IsAlive)
-                    consoleWriter.WritePositionToConsole(movingObjects[i].Position, i + 1);
-            }
+            for (int i = 0; i < movingObjects.Count; i++)
+                consoleWriter.WritePositionToConsole(movingObjects[i].Position, i + 1);
         }
     }
 
-    public class MovingObjectData
+    public class MovingObject
     {
         public Vector2 Position { get; private set; }
-        public bool IsAlive { get; private set; } = true;
 
-        public MovingObjectData(Vector2 startPosition)
+        public MovingObject(Vector2 startPosition)
         {
-            Position = ValidateCoordinates(startPosition);
+            Position = ClampCoordinates(startPosition);
         }
 
         public void ShiftPosition(Vector2 shift)
         {
-            Position = ValidateCoordinates(Position + shift);
+            Position = ClampCoordinates(Position + shift);
         }
 
-        public void KillObject()
-        {
-            IsAlive = false;
-        }
-
-        private Vector2 ValidateCoordinates(Vector2 position)
+        private Vector2 ClampCoordinates(Vector2 position)
         {
             if (position.x < 0)
                 position.x = 0;
@@ -135,5 +121,7 @@ namespace Task
         }
 
         public static Vector2 operator +(Vector2 f1, Vector2 f2) { return new Vector2(f1.x + f2.x, f1.y + f2.y); }
+        public static bool operator ==(Vector2 f1, Vector2 f2) { return f1.x == f2.x && f1.y == f2.y; }
+        public static bool operator !=(Vector2 f1, Vector2 f2) { return f1.x != f2.x && f1.y != f2.y; }
     }
 }
